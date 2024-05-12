4396c0d2a650d967d5540759a96a9906d2577127
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-05-12 18:46:09 +0100

    new useState num count

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header';
 import Menu from './component/Menu';
+import React from 'react';
 
 function App() {
+
+  const [num, setNum] = React.useState(0);
+
   return (
     <div className="App">
       <Menu />
       <Header logo={logo}/>
+      <div>
+        <p>{num}</p>
+        <button onClick={()=>setNum(num+1)}>Click</button>
+      </div>
     </div>
   );
 }
